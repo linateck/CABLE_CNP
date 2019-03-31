de60da394b15735085aa1f57e46df23b12bede73
mdekauwe <mdekauwe@gmail.com>
2019-03-31 22:50:10 +1100

    Fixed time stuff for new casa output

--- scripts/plot_casa_outputs_simulation.py
+++ scripts/plot_casa_outputs_simulation.py
@@ -17,42 +17,7 @@ import numpy as np
 from matplotlib.ticker import FixedLocator
 import os
 import xarray as xr
-
-def plot_inputs(cycle, ds):
-
-    fig = plt.figure(figsize=(15,6))
-    fig.subplots_adjust(hspace=0.3)
-    fig.subplots_adjust(wspace=0.3)
-    plt.rcParams['text.usetex'] = False
-    plt.rcParams['font.family'] = "sans-serif"
-    plt.rcParams['font.sans-serif'] = "Helvetica"
-    plt.rcParams['axes.labelsize'] = 12
-    plt.rcParams['font.size'] = 12
-    plt.rcParams['legend.fontsize'] = 12
-    plt.rcParams['xtick.labelsize'] = 12
-    plt.rcParams['ytick.labelsize'] = 12
-
-    ax1 = fig.add_subplot(1,3,1)
-    ax2 = fig.add_subplot(1,3,2)
-    ax3 = fig.add_subplot(1,3,3)
-
-    ax1.set_title("CO2")
-    ax1.plot(ds.time, ds.CO2air[:,0,0])
-
-    ax2.set_title("Ndep")
-    ax2.plot(ds.time, ds.Ndep[:,0,0])
-
-    ax1.set_title("Pdep")
-    ax1.plot(ds.time, ds.Pdep[:,0,0])
-
-    plot_fname = "%s_inputs.pdf" % (cycle)
-    plot_dir = "plots"
-    if not os.path.exists(plot_dir):
-        os.makedirs(plot_dir)
-
-    fig.tight_layout()
-    fig.savefig(os.path.join(plot_dir, plot_fname), bbox_inches='tight',
-                pad_inches=0.1)
+import glob
 
 def plot_carbon_fluxes(cycle, ds):
 
@@ -76,23 +41,23 @@ def plot_carbon_fluxes(cycle, ds):
     ax6 = fig.add_subplot(2,3,6)
 
     ax1.set_title("GPP/NPP")
-    ax1.plot(ds.time, ds.GPP[:,0,0], label="GPP")
-    ax1.plot(ds.time, ds.NPP[:,0,0], label="NPP")
+    ax1.plot(ds.time, ds.Cgpp[:,0], label="GPP")
+    ax1.plot(ds.time, ds.Cnpp[:,0], label="NPP")
     ax1.legend(numpoints=1, loc="best")
 
     ax2.set_title("CUE")
-    ax2.plot(ds.time, ds.NPP[:,0,0]/ds.GPP[:,0,0])
+    ax2.plot(ds.time, ds.Cnpp[:,0]/ds.Cgpp[:,0])
 
     ax3.set_title("LAI")
-    ax3.plot(ds.time, ds.LAI[:,0,0])
+    ax3.plot(ds.time, ds.glai[:,0])
 
     ax4.set_title("Vcmax")
     ax4.plot(ds.time, ds.vcmax[:,0]*1E6)
 
     ax5.set_title("Allocation")
-    ax5.plot(ds.time, ds.fracCallocLeaf[:,0,0], label="Af")
-    ax5.plot(ds.time, ds.fracCallocStem[:,0,0], label="Aw")
-    ax5.plot(ds.time, ds.fracCallocRoot[:,0,0], label="Ar")
+    ax5.plot(ds.time, ds.fracCalloc[:,0], label="Af")
+    ax5.plot(ds.time, ds.fracCalloc[:,1], label="Aw")
+    ax5.plot(ds.time, ds.fracCalloc[:,2], label="Ar")
     ax5.legend(numpoints=1, loc="best")
 
     ax6.set_title("C labile")
@@ -134,33 +99,33 @@ def plot_nitrogen_fluxes(cycle, ds):
 
 
     ax1.set_title("N fixation (g N/m^2/year)")
-    ax1.plot(ds.time, ds.Nfix[:,0], label="Nfix")
+    ax1.plot(ds.time, ds.Nminfix[:,0], label="Nfix")
 
     ax2.set_title("N deposition (g N/m^2/year)")
-    ax2.plot(ds.time, ds.Ndep[:,0])
+    ax2.plot(ds.time, ds.Nmindep[:,0])
 
     ax3.set_title("N loss (g N/m^2/year)")
-    ax3.plot(ds.time, ds.Nloss[:,0])
+    ax3.plot(ds.time, ds.Nminloss[:,0])
 
     ax4.set_title("N leach")
-    ax4.plot(ds.time, ds.Nleach[:,0])
+    ax4.plot(ds.time, ds.Nminleach[:,0])
 
     ax5.set_title("N uptake")
-    ax5.plot(ds.time, ds.Nup[:,0])
+    ax5.plot(ds.time, ds.Nupland[:,0])
 
     ax6.set_title("N gross mineralisation")
-    ax6.plot(ds.time, ds.Ngrossmin[:,0])
+    ax6.plot(ds.time, ds.Nsmin[:,0])
 
     ax7.set_title("N net mineralisation")
-    ax7.plot(ds.time, ds.Nnetmin[:,0])
+    ax7.plot(ds.time, ds.Nsnet[:,0])
 
     ax8.set_title("N immobilisation")
-    ax8.plot(ds.time, ds.Nimmob[:,0])
+    ax8.plot(ds.time, ds.Nsimm[:,0])
 
     ax9.set_title("Plant N:C")
-    ax9.plot(ds.time, ds.PlantNLeaf[:,0,0]/ds.PlantCarbLeaf[:,0,0], label="Leaf")
-    ax9.plot(ds.time, ds.PlantNWood[:,0,0]/ds.PlantCarbWood[:,0,0], label="Wood")
-    ax9.plot(ds.time, ds.PlantNRoot[:,0,0]/ds.PlantCarbFineRoot[:,0,0], label="Root")
+    ax9.plot(ds.time, ds.nplant[:,0]/ds.cplant[:,0], label="Leaf")
+    ax9.plot(ds.time, ds.nplant[:,1]/ds.cplant[:,1], label="Wood")
+    ax9.plot(ds.time, ds.nplant[:,2]/ds.cplant[:,2], label="Root")
     ax9.legend(numpoints=1, loc="best")
 
 
@@ -206,21 +171,21 @@ def plot_phosphorus_fluxes(cycle, ds):
     ax3.plot(ds.time, ds.Pleach[:,0])
 
     ax4.set_title("P uptake")
-    ax4.plot(ds.time, ds.Pup[:,0])
+    ax4.plot(ds.time, ds.Pupland[:,0])
 
     ax5.set_title("P gross mineralisation")
-    ax5.plot(ds.time, ds.Pgrossmin[:,0])
+    ax5.plot(ds.time, ds.Psmin[:,0])
 
     ax6.set_title("P net mineralisation")
-    ax6.plot(ds.time, ds.Psnetmin[:,0])
+    ax6.plot(ds.time, ds.Psnet[:,0])
 
     ax7.set_title("P immobilisation")
-    ax7.plot(ds.time, ds.Pimmob[:,0])
+    ax7.plot(ds.time, ds.Psimm[:,0])
 
-    ax8.set_title("Plant N:P")
-    ax8.plot(ds.time, ds.PlantNLeaf[:,0,0]/ds.PlantPLeaf[:,0,0], label="Leaf")
-    ax8.plot(ds.time, ds.PlantNWood[:,0,0]/ds.PlantPWood[:,0,0], label="Wood")
-    ax8.plot(ds.time, ds.PlantNRoot[:,0,0]/ds.PlantPRoot[:,0,0], label="Root")
+    ax8.set_title("Plant P:N")
+    ax8.plot(ds.time, ds.pplant[:,0]/ds.nplant[:,0], label="Leaf")
+    ax8.plot(ds.time, ds.pplant[:,1]/ds.nplant[:,1], label="Wood")
+    ax8.plot(ds.time, ds.pplant[:,2]/ds.nplant[:,2], label="Root")
     ax8.legend(numpoints=1, loc="best")
 
     plot_fname = "%s_simulation_phosphorus_fluxes.pdf" % (cycle)
@@ -236,17 +201,32 @@ def plot_phosphorus_fluxes(cycle, ds):
 
 if __name__ == "__main__":
 
+    met_dir = "/Users/mdekauwe/Desktop/CABLE_CNP/runs/met"
+    met_fname = glob.glob(os.path.join(met_dir, "*_simulation.nc"))[0]
+
+    ds_met = xr.open_dataset(met_fname)
+
+    df_met = ds_met.Tair.squeeze(dim=["x","y"], drop=True).to_dataframe()
+    df_met = df_met.reset_index()
+    df_met = df_met.set_index('time')
+    diff = df_met.index.minute[1] - df_met.index.minute[0]
+
     #for cycle in ["C", "CN", "CNP"]:
     for cycle in ["CN"]:
+        fname = "*_%s_out_casa_simulation.nc" % (cycle)
+        fname = glob.glob(os.path.join("outputs", fname))[0]
 
-        print(cycle)
-
-        #fname = "outputs/AU-Tum_2002-2010_OzFlux_Met_%s_out_cable_simulation.nc" % (cycle)
-        fname = "outputs/AU-Tum_%s_out_cable_simulation.nc" % (cycle)
-        #ds = xr.open_dataset(fname, decode_times=False)
         ds = xr.open_dataset(fname)
 
-        plot_inputs(cycle, ds)
+        # hour gap i.e. Tumba
+        if diff == 0:
+            time = pd.date_range(start=df_met.index[0], periods=len(ds.glai),
+                                 freq='D')
+        else:
+            time = pd.date_range(start=df_met.index[0], periods=len(ds.glai),
+                                 freq='D')
+        ds['time'] = time
+
         plot_carbon_fluxes(cycle, ds)
         plot_nitrogen_fluxes(cycle, ds)
-        #plot_phosphorus_fluxes(cycle, ds)
+        plot_phosphorus_fluxes(cycle, ds)
